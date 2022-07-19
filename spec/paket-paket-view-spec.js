ccf34943e166aacc98ea1abcdf9b99728341d101
hningswara <109612747+hningswara@users.noreply.github.com>
2022-07-20 00:28:09 +0700

    Add files via upload

---
+++ spec/paket-paket-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PaketPaketView from '../lib/paket-paket-view';
+
+describe('PaketPaketView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
